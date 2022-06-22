9b749bd825d0f3af55649f397384786dcb3b712a
Sid Dey <siddey@umich.edu>
2022-06-22 16:11:56 -0400

    tuning gain Kr in torque controller

--- ROAHM_Robust_Control/controllers/src/fetch_arm_torque_controller.cpp
+++ ROAHM_Robust_Control/controllers/src/fetch_arm_torque_controller.cpp
@@ -7,11 +7,13 @@ class FetchArmTorqueController {
     public:        
         Eigen::VectorXd q_, qd_, q_des_, qd_des_, qdd_des_;
         unsigned int num_joints_;
-        Eigen::MatrixXd Kr_;
+        // Eigen::MatrixXd Kr_;
+        Eigen::DiagonalMatrix<double, 7> Kr_;
 
     FetchArmTorqueController() {
         num_joints_ = 7;
-        Kr_ = Eigen::MatrixXd::Identity(num_joints_, num_joints_) * 20 ;
+        // Kr_ = Eigen::MatrixXd::Identity(num_joints_, num_joints_) * 20 ;
+        Kr_.diagonal() << 20, 50, 20, 20, 20, 20, 20 ;
 
         //Resize input vectors to the controller
         q_.resize(num_joints_);
